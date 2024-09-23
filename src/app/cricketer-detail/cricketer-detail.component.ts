10e3080168960679f646304cba40ceecc7d29e50
Sakshi Patel <w0860745@myscc.ca>
2024-09-23 14:11:16 -0400

    Created 2 new components

---
+++ src/app/cricketer-detail/cricketer-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cricketer-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './cricketer-detail.component.html',
+  styleUrl: './cricketer-detail.component.css'
+})
+export class CricketerDetailComponent {
+
+}
---
+++ src/app/cricketer-list/cricketer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cricketer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './cricketer-list.component.html',
+  styleUrl: './cricketer-list.component.css'
+})
+export class CricketerListComponent {
+
+}
