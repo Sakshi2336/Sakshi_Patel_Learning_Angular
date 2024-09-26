4502b73c25722e13419247001a0c00fac6ba372e
Sakshi Patel <w0860745@myscc.ca>
2024-09-26 19:11:15 -0400

    Moved array to mock file and created new CricketPlayer service

--- src/app/cricketer-list/cricketer-list.component.ts
+++ src/app/cricketer-list/cricketer-list.component.ts
@@ -12,17 +12,6 @@ import {CricketerListItemComponent} from "../cricketer-list-item/cricketer-list-
 })
 export class CricketerListComponent {
 
-  cricketersList: Cricketer[] = [
-    {id: 1, firstName: 'Virat', lastName: 'Kohli', country: 'India', role: 'Batsman', isActive: true},
-    {id: 2, firstName: 'Steve', lastName: 'Smith', country: 'Australia', role: 'Batsman', isActive: true},
-    {id: 3, firstName: 'Rashid', lastName: 'Khan', country: 'Afghanistan', role: 'Bowler', isActive: true},
-    {id: 4, firstName: 'Ben', lastName: 'Stokes', country: 'England', role: 'All-Rounder', isActive: true},
-    {id: 5, firstName: 'Kane', lastName: 'Williamson', country: 'New Zealand', role: 'Batsman', isActive: false},
-    {id: 6, firstName: 'AB', lastName: 'de Villiers', country: 'South Africa', role: 'Batsman', isActive: false}
-  ]
-
-
-
 //Not Needed Code Below. You can remove it - Matt
   //function
   selectedCricketer?: Cricketer;
