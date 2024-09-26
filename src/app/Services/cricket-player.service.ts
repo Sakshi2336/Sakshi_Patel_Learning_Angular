cfa42cc613e4c2035f481254c4936dc4bfa4e250
Sakshi Patel <w0860745@myscc.ca>
2024-09-26 19:19:32 -0400

    Create method for array in service and add dependency injection in list component.ts

--- src/app/Services/cricket-player.service.ts
+++ src/app/Services/cricket-player.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Cricketer} from "../Shared/Modules/cricketer";
+import {cricketersList} from "../Shared/mockCricketer.data";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CricketPlayerService {
 
+  //local copy of cricketer list
+  private cricketer:Cricketer[] = cricketersList;
   constructor() { }
+
+  //this method will return the array from mock file
+  getCricketer():Observable<Cricketer[]>{
+    return of (cricketersList);
+  }
 }
--- src/app/cricketer-list/cricketer-list.component.ts
+++ src/app/cricketer-list/cricketer-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Cricketer} from "../Shared/Modules/cricketer";
 import {NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 import {CricketerListItemComponent} from "../cricketer-list-item/cricketer-list-item.component";
+import {CricketPlayerService} from "../Services/cricket-player.service";
 
 @Component({
   selector: 'app-cricketer-list',
@@ -11,6 +12,13 @@ import {CricketerListItemComponent} from "../cricketer-list-item/cricketer-list-
   styleUrl: './cricketer-list.component.css'
 })
 export class CricketerListComponent {
+
+  //I still want a local copy of the cricketer list
+  cricketerList:Cricketer[] = [];
+
+  //calling our service
+  constructor(private cricketPlayerService : CricketPlayerService) {
+  }
 
 //Not Needed Code Below. You can remove it - Matt
   //function
