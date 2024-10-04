2d4e506b7f58fee41aab156d2edf9cfaefe21a11
Sakshi Patel <w0860745@myscc.ca>
2024-10-04 14:09:39 -0400

    Define the list of routers in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter,Routes} from "@angular/router";
+import {CricketerListComponent} from "./app/cricketer-list/cricketer-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path:'', redirectTo: '/cricketers', pathMatch: 'full'}, //default route
+  { path: 'cricketers', component: CricketerListComponent },
+  { path: 'cricketers/:id', component: CricketerListComponent },
+  {path:'modify-cricketer', component: ModifyStudentComponent},
+  {path: '**', component:PageNotFoundComponent}//Wildcard route for a 404 page
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
