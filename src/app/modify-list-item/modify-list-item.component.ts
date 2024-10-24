9df8cf3f0bcf6a92794cb2fa8de2243ce98bff0a
Sakshi Patel <w0860745@myscc.ca>
2024-10-24 19:16:29 -0400

    Step 3,4 and 5 completed

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {CricketPlayerService} from "../Services/cricket-player.service";
 
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
 export class ModifyListItemComponent {
+
+  // form group
+  cricketerForm : FormGroup;
+
+  //Adding form builder and validators
+  constructor(
+    private fb:FormBuilder,
+    private route : ActivatedRoute,
+    private cricketerService : CricketPlayerService,
+    private router : Router
+  ) {
+    this.cricketerForm = this.fb.group({
+      id : ['',Validators.required],
+      firstName : ['',Validators.required],
+      lastName : ['',Validators.required],
+      role : ['',Validators.required],
+      isAdmin : ['',Validators.required]
+    })
+  }
+
 
 }
