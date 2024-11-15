9f409ab9da5de040cabdf5f5e4a09339a04e38c0
Sakshi Patel <w0860745@myscc.ca>
2024-11-14 19:53:56 -0500

    Add input field for age

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -33,6 +33,7 @@ export class ModifyListItemComponent implements OnInit{
       id : [cricketerService.generateNewId()],
       firstName : [''],
       lastName : [''],
+      age : [''],
       country : [''],
       dob : [''],
       netWorth : [''],
